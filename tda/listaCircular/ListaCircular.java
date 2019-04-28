e749aa98ea37a9094e72629eb9f35f36a279d3d0
Sebastian Rossi <35445409+sebastardo@users.noreply.github.com>
2019-04-28 16:03:42 -0300

    Correcion comentario

--- tda/listaCircular/ListaCircular.java
+++ tda/listaCircular/ListaCircular.java
@@ -4,7 +4,7 @@ import tda.nodoSimple.Nodo;
 
 /**
  * Lista Circular.
- * La idea era hacerlo si usar Cola ni Pila, pero me canse y deberia hacer otras cosas
+ * La idea era hacerlo sin usar Cola ni Pila, pero me canse y deberia hacer otras cosas
  * Utiliza nodo simple.
  * @author Sebastian Rossi
  */
